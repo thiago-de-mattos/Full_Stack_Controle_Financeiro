a29c840a8ed7bdbf9661b9d24981df620b704dab
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-09-09 21:16:49 -0300

    feat: cria apps

--- finace/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class FinaceConfig(AppConfig):
-    name = "finace"
---
+++ finance/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FinacenConfig(AppConfig):
+    name = "finance"
