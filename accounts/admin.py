9d50e5308811f9e389c04c36df33f087da75ca10
thiago de mattos <205856258+thiago-de-mattos@users.noreply.github.com>
2026-09-23 18:41:34 -0300

    feat(accounts): configura admin do usuário customizado

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ["email"]
+    list_display = ["email", "name", "is_staff", "is_active", "date_joined"]
+    list_filter = ["is_staff", "is_superuser", "is_active"]
+    search_fields = ["email", "name"]
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Dados pessoais", {"fields": ("name", "birth_date")}),
+        (
+            "Permissões",
+            {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")},
+        ),
+        ("Datas", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "name", "password1", "password2"),
+            },
+        ),
+    )
